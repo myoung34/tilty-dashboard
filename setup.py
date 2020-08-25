9d4059d1eb4784d23c41963fb0dad6d59f88e6ab
Marcus Young <myoung34@my.apsu.edu>
2020-08-25 06:27:03 -0500

    Bump version 1.1.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author_email='3vilpenguin@gmail.com',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version='1.1.2',
+    version='1.1.3',
     package_data={
        "": ["*.html", "*.png", "*.css", "*.js"],
     },
